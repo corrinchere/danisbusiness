dcdb0a0f3bab10be0e37fed7e8e0546c8dcc167e
corrinchere <96142555+corrinchere@users.noreply.github.com>
2022-01-09 23:42:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+console.log("Let's do this")
+
+// const bombButton = document.getElementById("example-bomb")
+// bombButton.addEventListener("dblclick", function(mineBomb){
+//     document.getElementById("example-bomb").style.backgroundColor = "red";
+//      document.getElementById("example-bomb").innerHTML = '<img src="images/icons8-bomb-64.png"/>';
+//  })
+
+
+const changeblurb = document.getElementById("bookkeeping")
+
+changeblurb.addEventListener("click", function(changeimage){
+    document.getElementById("offer-statement").innerHTML = "THIS IS ABOUT BOOKKEEPING. THIS IS ABOUT BOOKKEEPING. THIS IS ABOUT BOOKKEEPING. THIS IS ABOUT BOOKKEEPING. THIS IS ABOUT BOOKKEEPING. THIS IS ABOUT BOOKKEEPING. THIS IS ABOUT BOOKKEEPING."
+})
+
+const changeapar = document.getElementById("ap-ar")
+
+changeapar.addEventListener("click", function(changeimage2){
+    document.getElementById("offer-statement").innerHTML = "THIS IS ABOUT AP/AR. THIS IS ABOUT AP/AR. THIS IS ABOUT AP/AR. THIS IS ABOUT AP/AR. THIS IS ABOUT AP/AR. THIS IS ABOUT AP/AR. THIS IS ABOUT AP/AR."
+})
+
+const changereconcil = document.getElementById("reconcil")
+
+changereconcil.addEventListener("click", function(changeimage3){
+    document.getElementById("offer-statement").innerHTML = "THIS IS ABOUT reconcil. THIS IS ABOUT reconcil. THIS IS ABOUT reconcil. THIS IS ABOUT reconcil. THIS IS ABOUT reconcil. THIS IS ABOUT reconcil. THIS IS ABOUT reconcil."
+})
+
+const changeconsult = document.getElementById("consult")
+
+changeconsult.addEventListener("click", function(changeimage4){
+    document.getElementById("offer-statement").innerHTML = "THIS IS ABOUT consult. THIS IS ABOUT consult. THIS IS ABOUT consult. THIS IS ABOUT consult. THIS IS ABOUT consult. THIS IS ABOUT consult. THIS IS ABOUT consult."
+})
+
+const changestatements = document.getElementById("statements")
+
+changestatements.addEventListener("click", function(changeimage5){
+    document.getElementById("offer-statement").innerHTML = "THIS IS ABOUT statments. THIS IS ABOUT statements. THIS IS ABOUT statements. THIS IS ABOUT statements. THIS IS ABOUT statements. THIS IS ABOUT statements. THIS IS ABOUT statements."
+})
+
+const changebilling = document.getElementById("billing")
+
+changebilling.addEventListener("click", function(changeimage6){
+    document.getElementById("offer-statement").innerHTML = "THIS IS ABOUT billing. THIS IS ABOUT billing. THIS IS ABOUT billing. THIS IS ABOUT billing. THIS IS ABOUT billing. THIS IS ABOUT billing. THIS IS ABOUT billing."
+})
+
